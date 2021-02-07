b455abe00813a1a1dbaaeba8a80cf20a4228e493
vaibhav <chandravaibhav65@gmail.com>
2021-02-07 16:40:20 +0530

    priliminary footer

--- src/Home/components/Title.js
+++ src/Home/components/Title.js
@@ -4,8 +4,9 @@ import logo from "../../assets/logo.png";
 export const Title = () => {
 	return (
 		<div>
-			<img src={logo} alt="logo" />
-			<h1>International Relations</h1>
+			title
+			{/* <img src={logo} alt="logo" />
+			<h1>International Relations</h1> */}
 		</div>
 	);
 };
