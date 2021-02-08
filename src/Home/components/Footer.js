f1942310149bf08b5ae006444d4816e87dc1813a
Kushaan Gowda <kushaangowda@yahoo.com>
2021-02-08 11:53:12 +0530

    fixed some stuff

--- src/Home/components/Footer.js
+++ src/Home/components/Footer.js
@@ -1,50 +1,49 @@
 import React from "react";
 import img from "../../assets/iiti_logo.png";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 export const Footer = () => {
 	return (
 		<div className="footer">
 			<Container fluid className="footergrid">
 				<Row>
-					<Col xs={12} lg={3} >
+					<Col xs={12} lg={3}>
 						<div className="logo">
 							<img src={img} />
 						</div>
 					</Col>
 					<Col className="footerdesc" xs={12} lg={9}>
-
 						<div class="doclinks">
-							<a href="#"> Doc1 </a>|
-										<a href="#"> Doc2 </a>|
-										<a href="#"> Doc3 </a>
+							<a href="#"> Doc1 </a>|<a href="#"> Doc2 </a>|<a href="#"> Doc3 </a>
 						</div>
 
 						<Container className="addphone">
-							<Row >
+							<Row>
 								<Col xs={12} lg={4} className="address">
-									<div className="addresstext">
-										Address: Khandwa Rd, Simrol, Madhya Pradesh 453552
-										</div>
-								</Col  >
+									<div className="addresstext">Address: Khandwa Rd, Simrol, Madhya Pradesh 453552</div>
+								</Col>
 								<Col xs={12} lg={4} className="phone">
-									<div className="phonetext">
-										Phone No. - 1234567890
-										</div>
+									<div className="phonetext">Phone No. - 1234567890</div>
 								</Col>
 								<Col className="social" xs={12} lg={4}>
-									<div >
-										<a href="#" target="_blank"><i class="fab fa-github"></i></a>
-										<a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
-										<a href="#/" target="_blank"><i class="fab fa-facebook-f"></i></a>
-										<a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+									<div>
+										<a href="#" target="_blank">
+											<i class="fab fa-instagram"></i>
+										</a>
+										<a href="#/" target="_blank">
+											<i class="fab fa-facebook-f"></i>
+										</a>
+										<a href="#" target="_blank">
+											<i class="fab fa-linkedin-in"></i>
+										</a>
+										<a href="#" target="_blank">
+											<i class="far fa-envelope"></i>
+										</a>
 									</div>
 								</Col>
 							</Row>
 						</Container>
-
 					</Col>
-
 				</Row>
 			</Container>
 		</div>
--- src/Home/components/Title.js
+++ src/Home/components/Title.js
@@ -7,23 +7,35 @@ export const Title = () => {
 	return (
 		<div className="Title">
 			<p className="titleCollege">
-				<div className="container-fluid">Indian Institute of Technology, Indore</div>
+				<div className="container-fluid">
+					Indian Institute of Technology, Indore
+					<a href="#" className="socLink" target="_blank">
+						<i class="far fa-envelope"></i>
+					</a>
+					<a href="#" className="socLink" target="_blank">
+						<i class="fab fa-linkedin-in"></i>
+					</a>
+					<a href="#" className="socLink" target="_blank">
+						<i class="fab fa-instagram"></i>
+					</a>
+					<a href="#" className="socLink" target="_blank">
+						<i class="fab fa-facebook-f"></i>
+					</a>
+				</div>
 			</p>
 			<Navbar bg="light" variant="light" className="ba">
 				<div className="container-fluid">
 					<Navbar.Brand href="#home">
 						<img src={logo} alt="logo" className="titleLogo" />
 					</Navbar.Brand>
-					{/*
-						<Navbar.Toggle />
-						<Navbar.Collapse className="justify-content-end">
-							<Navbar.Text>
-								<h2 className="titleText" id="titleText1">
-									International Relations
-								</h2>
-							</Navbar.Text>
-						</Navbar.Collapse>
-					*/}
+					<Navbar.Toggle />
+					<Navbar.Collapse className="justify-content-end">
+						<Navbar.Text>
+							<h2 className="titleText" id="titleText1">
+								International Cell
+							</h2>
+						</Navbar.Text>
+					</Navbar.Collapse>
 				</div>
 			</Navbar>
 		</div>
