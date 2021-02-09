4f6a5ef787c992229eebcc5d74ee46ded290a436
Kushaan Gowda <kushaangowda@yahoo.com>
2021-02-09 10:05:15 +0530

    fixed some stuff

--- src/Home/components/News.js
+++ src/Home/components/News.js
@@ -1,22 +1,61 @@
-import React from "react";
-import $ from 'jquery';
-import OwlCarousel from 'react-owl-carousel';
+import React, { useState } from "react";
+// import $ from "jquery";
+import OwlCarousel from "react-owl-carousel";
 
-import 'owl.carousel/dist/assets/owl.carousel.css';
+import "owl.carousel/dist/assets/owl.carousel.css";
 
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import Newsutil from "../reusables/Newsutil";
 
 export const News = () => {
-	let title = 'Coronavirus Lockdown';
-	let text = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+	const [news, setNews] = useState([
+		{
+			title: "Coronavirus Lockdown 1",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+		{
+			title: "Coronavirus Lockdown 2",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+		{
+			title: "Coronavirus Lockdown 3",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+		{
+			title: "Coronavirus Lockdown 4",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+		{
+			title: "Coronavirus Lockdown 5",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+		{
+			title: "Coronavirus Lockdown 6",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+		{
+			title: "Coronavirus Lockdown 7",
+			text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+			date: "January 27, 2021",
+		},
+	]);
+
+	// let title = "Coronavirus Lockdown";
+	// let text = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+	// let date = "January 27, 2021";
 
 	return (
-		<div class="news container bg-light mt-5" >
-			<div class='container-fluid'>
+		<div class="news container bg-light mt-5">
+			<div class="container-fluid">
 				<h1 className="p-2 pl-5 pt-5 pr-5 mainTitle">News</h1>
 			</div>
-			<div class='container-fluid' style={{ maxWidth: '1140px' }} >
+			<div class="container-fluid" style={{ maxWidth: "1140px" }}>
 				<OwlCarousel
 					className="owl-theme"
 					loop
@@ -25,27 +64,39 @@ export const News = () => {
 					responsive={{
 						0: {
 							items: 1,
-
 						},
 						760: {
 							items: 2,
-
 						},
 						1000: {
 							items: 3,
-
-						}
+						},
 					}}
 				>
-					<div ><Newsutil text={text} title={title} /></div>
-					<div><Newsutil text={text} title={title} /></div>
-					<div><Newsutil text={text} title={title} /></div>
-					<div><Newsutil text={text} title={title} /></div>
-					<div><Newsutil text={text} title={title} /></div>
-					<div><Newsutil text={text} title={title} /></div>
-					<div><Newsutil text={text} title={title} /></div>
+					<Newsutil news={news} />
+					{/*<div>
+						<Newsutil text={text} date={date} title={title + " 1"} />
+					</div>
+					<div>
+						<Newsutil text={text} date={date} title={title + " 2"} />
+					</div>
+					<div>
+						<Newsutil text={text} date={date} title={title + " 3"} />
+					</div>
+					<div>
+						<Newsutil text={text} date={date} title={title + " 4"} />
+					</div>
+					<div>
+						<Newsutil text={text} date={date} title={title + " 5"} />
+					</div>
+					<div>
+						<Newsutil text={text} date={date} title={title + " 6"} />
+					</div>
+					<div>
+						<Newsutil text={text} date={date} title={title + " 7"} />
+					</div>*/}
 				</OwlCarousel>
 			</div>
 		</div>
-	)
+	);
 };
--- src/Home/reusables/Newsutil.js
+++ src/Home/reusables/Newsutil.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import { Card, Title, Text, Subtitle, Link, Body } from 'react-bootstrap';
-import img from '../../assets/image2.jpg'
-export default function ({ text, title }) {
-
-    return (
-        <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={img} />
-                <Card.Body>
-                    <Card.Title style={styles.title}><a href='#' >{title}</a></Card.Title>
-                    <Card.Text style={styles.text} >
-                        {text}
-                    </Card.Text>
-
-                </Card.Body>
-            </Card>
-        </div>
-    )
+import React from "react";
+import { Card } from "react-bootstrap";
+import img from "../../assets/image2.jpg";
+export default function ({ news }) {
+	return news.map((News) => {
+		return (
+			<div>
+				<Card style={{ width: "18rem" }}>
+					<div className="img-holder">
+						<Card.Img variant="top" src={img} />
+					</div>
+					<Card.Body>
+						<Card.Title style={styles.title}>
+							<a href="#">{News["title"]}</a>
+						</Card.Title>
+						<small className="text-muted">{News["date"]}</small>
+						<Card.Text style={styles.text}>{News["text"]}</Card.Text>
+					</Card.Body>
+				</Card>
+			</div>
+		);
+	});
 }
 
 const styles = {
-    title: {
-        fontWeight: 'bolder',
-        borderBottom: '2px solid #e2e2e2 ',
-        width: 'fit-content'
-    },
-    text: {
-        marginTop: '15%'
-    }
-}
+	title: {
+		fontWeight: "bolder",
+		borderBottom: "2px solid #e2e2e2 ",
+		width: "fit-content",
+	},
+	text: {
+		marginTop: "15%",
+		textAlign: "justify",
+	},
+};
