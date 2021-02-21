0c80a1b3d4fe1990224406fdedd224c566c39651
Kushaan Gowda <kushaangowda@yahoo.com>
2021-02-21 20:07:50 +0530

    removed app.css

--- src/Home/components/Events/Events.js
+++ src/Home/components/Events/Events.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../../../App.css";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
