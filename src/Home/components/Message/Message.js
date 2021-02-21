250f08fd491889bb65dd37867890a7fa1a55c471
Kushaan Gowda <kushaangowda@yahoo.com>
2021-02-21 20:15:13 +0530

    fixed heading font size and image size in mobile view

--- src/Home/components/Message/Message.js
+++ src/Home/components/Message/Message.js
@@ -8,10 +8,10 @@ export default function Message() {
 				<div className="container-fluid pb-5">
 					<div className="card-group ">
 						<div className="card bg-light col-sm-12 border-0">
-							<h1 className="p-2 pl-5 pt-5 pr-5 mainTitle" style={{ margin: "auto" }}>
+							<h1 className="p-2 pt-5 mainTitle" style={{ margin: "auto" }}>
 								Message From The Dean
 							</h1>
-							<img src={image} className="img-thumbnail" alt="img-responsive" style={{ width: "25%", margin: "auto" }} />
+							<img src={image} className="img-thumbnail" alt="img-responsive" style={{ width: "min(70vw,300px)", margin: "auto" }} />
 							<div className="card-body" style={{ textAlign: "center" }}>
 								<p className="mainDesc">
 									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
