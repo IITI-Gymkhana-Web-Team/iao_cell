4dd25c39d73304d5cf8cbcdfc906b689e8c937aa
khushi <khushipatidar9@gmail.com>
2021-02-28 18:38:25 +0530

    background added of image in about us

--- src/Home/pages/Aboutuspage/Aboutiiti.js
+++ src/Home/pages/Aboutuspage/Aboutiiti.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { image14 } from "../../../assets";
-import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
+import "./Aboutiiti.css";
 
 export default function Aboutiiti() {
 	return (
 		<>
 			<div className="container bg-light mt-5 mb-5" style={{ borderRadius: "10px" }}>
 				<h1 className="p-2 pl-5 pt-5 pr-5 mainTitle" style={{ textAlign: "center" }}>About IIT Indore</h1>
-				<div className="container-fluid pb-5">
+				<div className="container-fluid">
 					<div className="card-group ">
-						<div className="card border-0 bg-light" style={{ alignItems: "center" }}>
-							<img style={{ width: "600px", height: "400px" }} src={image14} className="img-thumbnail" alt="img-responsive" />
+						<div className="imagecard border-0 bg-light">
+							<img style={{ width: "600px", height: "400px" }} src={image14} className="image img-thumbnail" alt="img-responsive" />
 						</div>
 						<p className="" style={{ textAlign: "center" }}>
 							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
@@ -22,7 +22,7 @@ export default function Aboutiiti() {
 								</p>
 					</div>
 				</div>
-				<div style={{textAlign:"center"}}><p style={{fontSize:"1.6rem"}}>For more details:- <a href="http://iiti.ac.in/" target="_blank"><button className="btn btn-primary">Visit</button></a></p></div>
+				<div style={{textAlign:"center"}} className="mt-2 pb-3"><p style={{fontSize:"1.6rem"}}>For more details:- <a href="http://iiti.ac.in/" target="_blank"><button className="btn btn-primary">Visit</button></a></p></div>
 			</div>
 		</>
 	)
