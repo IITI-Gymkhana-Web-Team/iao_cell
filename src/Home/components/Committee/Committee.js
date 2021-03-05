524acd937b1e360a8e32a322ccb5cae8957c8521
Kushaan Gowda <kushaangowda@yahoo.com>
2021-03-05 17:19:48 +0530

    added hierarchy thingy to people's page

--- src/Home/components/Committee/Committee.js
+++ src/Home/components/Committee/Committee.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { PeopleCard } from "../PeopleCard/PeopleCard";
+import { Row } from "react-bootstrap";
+import "./Committee.css";
 
 export const Committee = ({ committee }) => {
 	return (
@@ -8,8 +10,14 @@ export const Committee = ({ committee }) => {
 				<h1 className="p-3 m-2 mainTitle text-center">{committee.title}</h1>
 			</div>
 			<div className="peopleCards">
-				{committee.members.map((person) => {
-					return <PeopleCard person={person} key={person.email} />;
+				{committee.members.map((persons) => {
+					return (
+						<Row>
+							{persons.map((person) => {
+								return <PeopleCard person={person} key={person.email} />;
+							})}
+						</Row>
+					);
 				})}
 			</div>
 		</>
