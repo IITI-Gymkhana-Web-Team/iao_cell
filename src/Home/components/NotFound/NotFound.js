e80d5bfde180eb75da464bf867559b4a93d1156a
Kushaan Gowda <kushaangowda@yahoo.com>
2021-03-23 12:36:05 +0530

    added not found page

--- src/Home/components/NotFound/NotFound.js
+++ src/Home/components/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./NotFound.css";
+import { Link } from "react-router-dom";
 
 export const NotFound = () => {
 	return (
-		<div>
-			<p>404 Error</p>
-			<p>Page not found</p>
+		<div className="NotFound">
+			<div className="container font-ubuntu">
+				<h1>Oops!</h1>
+				<h2 className="pt-5 font-acme">404 Error</h2>
+				<h5>Looks like the page which you were trying to find does not exist</h5>
+				<Link to="/">Back To Home Page</Link>
+			</div>
 		</div>
 	);
 };
