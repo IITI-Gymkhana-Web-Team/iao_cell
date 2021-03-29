e8bd7ee40b745092db42342da32a4c3dec32a44d
Kushaan Gowda <kushaangowda@yahoo.com>
2021-03-29 14:32:40 +0530

    fixed grammar stuff of life at iiti and about iiti page

--- src/Home/components/AboutIITI/AboutIITI.js
+++ src/Home/components/AboutIITI/AboutIITI.js
@@ -8,72 +8,64 @@ export const AboutIITI = () => {
 			<h1 className="mainTitle text-center m-3">About IITI</h1>
 			<img src={iiti} style={{ float: "right", maxWidth: "max(400px,45%)" }} />
 			<p>
-				In keeping with India’s vision to become a world leader in Science and Technology
-				and to usher in a new revolution, resulting in an unprecedented economic growth,
-				Government of India reassessed the need of technical manpower and decided to set up
-				eight new IITs. Six of them started functioning back from the academic year 2008-09.
-				These were established at Hyderabad, Gandhinagar, Rajasthan, Ropar, Patna and
-				Bhubaneswar. IIT Indore and IIT Mandi started functioning from July 2009.
+				In keeping with India's vision to become a world leader in Science and Technology
+				and to usher in a new revolution, resulting in unprecedented economic growth, the
+				Government of India reassessed the need for a technical workforce and decided to set
+				up eight new IITs. Six of them started functioning back from the academic year
+				2008-09. These were established at Hyderabad, Gandhinagar, Rajasthan, Ropar, Patna,
+				and Bhubaneswar. IIT Indore and IIT Mandi started functioning in July 2009. Indian
+				Institute of Technology Indore, located in Madhya Pradesh, known as IIT Indore or
+				IITI, is an institute of national importance established by the Government of India
+				in 2009.
 			</p>
 			<p>
-				Indian Institute of Technology Indore located in Madhya Pradesh, known as IIT Indore
-				or IITI, is an institute of national importance established by the Government of
-				India in 2009. It is one of the eight new IITs, started by the Ministry of Human
+				Indian Institute of Technology Indore, located in Madhya Pradesh, known as IIT
+				Indore or IITI, is an institute of national importance established by the Government
+				of India in 2009. It is one of the eight new IITs, started by the Ministry of Human
 				Resource Development (India), Government of India. The institution started
-				functioning from 2009-10 in a temporary campus at Institute of Engineering and
-				Technology of Devi Ahilya University, Indore under the mentorship of IIT Bombay.
-				Shri Arjun Singh, the Union HRD minister, laid the foundation of the permanent
-				campus, spread over an area of around 501.42-acre (2.1 sq. km), on 17th February
-				2009 at Simrol, a location on Khandwa Road about 25 km from the city of Indore.
-			</p>
-			<p>
-				Indian Institute of Technology Indore located in Madhya Pradesh, known as IIT Indore
-				or IITI, is an institute of national importance established by the Government of
-				India in 2009. It is one of the eight new IITs, started by the Ministry of Human
-				Resource Development (India), Government of India. The institution started
-				functioning from 2009-10 in a temporary campus at Institute of Engineering and
-				Technology of Devi Ahilya University, Indore under the mentorship of IIT Bombay.
-				Shri Arjun Singh, the Union HRD minister, laid the foundation of the permanent
-				campus, spread over an area of around 501.42-acre (2.1 sq. km), on 17th February
-				2009 at Simrol, a location on Khandwa Road about 25 km from the city of Indore.
+				functioning from 2009-10 on a temporary campus at the Institute of Engineering and
+				Technology of Devi Ahilya University, Indore, under IIT Bombay's mentorship. Shri
+				Arjun Singh, the Union HRD minister, laid the foundation of the permanent campus,
+				spread over 501.42-acre (2.1 sq. km), on 17th February 2009 at Simrol, a location on
+				Khandwa Road about 25 km from the city of Indore.
 			</p>
 			<p>
 				As IIT Indore steps into the next decade, it carries forward the culture of
 				excellence and hard work embedded in its social environment. It maintains a high
-				national rank among the institutions of higher education and is now among the top 10
+				national rank among the institutions of higher education. It is now among the top 10
 				engineering institutes in the country. Over the last eight years, our graduating
-				students have been engaged with the best corporations, in India and worldwide,
-				across different verticals. Many have also chosen the less travelled path of
+				students have been engaged with the best corporations in India and worldwide, across
+				different verticals. Many have also chosen the less-traveled path of
 				entrepreneurship. It is indeed a proud moment for us when our students contribute to
-				the society by creating jobs and changing lives. Our faculty colleagues, along with
-				their researchers in the last one decade, have also shown immense perseverance to
-				attain new heights of excellence.
+				society by creating jobs and changing lives. Along with their researchers in the
+				last decade, our faculty colleagues have also shown immense perseverance to attain
+				new heights of excellence.
 			</p>
 			<img
 				src={iiti2}
 				style={{ float: "left", maxWidth: "max(400px,45%)", marginRight: "10px" }}
 			/>
 			<p>
-				The teaching and research climate in the institute has been supported by efficient
-				administration. Having been granted its first patent, the institute has renewed its
+				The teaching and research climate in the Institute has been supported by efficient
+				administration. Having been granted its first patent, the Institute has renewed its
 				focus on patenting along with publishing. The key focus is to engage with MSMEs,
-				start-ups and transfer technology. Efforts are also underway towards liaison and
-				collaboration with grassroot-level organizations, to contribute to the development
-				of our immediate neighborhood through technological innovation. I, on behalf of the
-				Institute, congratulate all our graduates and wish them success in the future. 2020
-				is no ordinary year – the normal has been redefined worldwide in a way that has
-				posed major challenges for the education sector.
+				start-ups, and transfer technology. Efforts are also underway towards liaison and
+				collaboration with grassroots-level organizations to contribute to our immediate
+				neighborhood's development through technological innovation. On behalf of the
+				Institute, I congratulate all our graduates and wish them success in the future.
+				2020 is no ordinary year – the normal has been redefined worldwide to pose
+				significant challenges for the education sector.
 			</p>
 			<p>
-				The IIT Indore community has responded by innovating in all the domains, across
-				teaching, research, and administration. The institute used asynchronous as well as
-				synchronous modes of teaching, successfully completed the Spring semester in a
-				timely manner, and continued with academic activities. The research on much needed
-				COVID-19 related projects also continued unabated during the lockdown. We initiated
-				the return of researchers to the campus, with all the due precautions for everyone’s
-				safety, to restart the laboratories. Even during such trying times, our
-				infrastructure team continued the hard work and did not allow any compromise on the
-				high standards the institute has set for itself.
+				The IIT Indore community has responded by innovating in all the domains across
+				teaching, research, and administration. The Institute used asynchronous and
+				synchronous modes of instruction, completed the Spring semester promptly, and
+				continued with academic activities. The research on much-needed COVID-19 related
+				projects also continued unabated during the lockdown. We initiated the return of
+				researchers to the campus, with all the due precautions for everyone's safety, to
+				restart the laboratories. Even during such trying times, our infrastructure team
+				continued the hard work. It did not allow any compromise on the high standards the
+				Institute has set for itself.
 			</p>
 			<h3 className="font-acme mt-5">Citations and H Index</h3>
 			<div className="charts">
