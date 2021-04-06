e7becdc1f90b150e38ba646b75209dfdbdb47acd
gg-dev-05 <garvitgalgat@gmail.com>
2021-04-06 08:10:57 +0530

    opportunites data

--- src/Home/components/NewNav/DropdownContents/OpportunitiesDropdown.js
+++ src/Home/components/NewNav/DropdownContents/OpportunitiesDropdown.js
@@ -3,83 +3,115 @@ import styled from "styled-components";
 import { Icon, DropdownSection, Heading, HeadingLink } from "./Components";
 import { Link } from "react-router-dom";
 
-const OpportunitiesDropdownEl = styled.div`
-	width: 15rem;
-`;
-
-const Logo = styled.div`
-	width: 38px;
-	height: 38px;
-	margin-right: 16px;
-	border-radius: 100%;
-	opacity: 0.6;
-	background-color: ${({ color }) => `var(--${color})`};
-`;
-
-const SubProductsList = styled.ul`
-	li {
-		display: flex;
-		margin-bottom: 1rem;
-	}
-	h3 {
-		margin-right: 1rem;
-		width: 3.2rem;
-		display: block;
-	}
-	a {
-		color: var(--dark-grey);
-	}
-`;
-
-const ProductsSection = styled.ul`
-	li {
-		display: flex;
-	}
-`;
-
-const WorksWithStripe = styled.div`
- border-top: 2px solid #fff;
-  display:flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: var(--spacer);
-  padding-top: var(--spacer);
-}
-h3 {
-  margin-bottom: 0;
-}
-`;
-
 const OpportunitiesDropdown = () => {
 	return (
-		<OpportunitiesDropdownEl>
-			<DropdownSection data-first-dropdown-section>
-				<ul>
-					<HeadingLink>
-						<Icon /> Student
-					</HeadingLink>
-					<div className="d-flex mobDropdown">
-						<DropdownSection>
-							<li>
-								<Link to="/">Internships</Link>
-							</li>
-							<li>
-								<Link to="/opportunities/pg_phd_post_doc">PG/PhD/Post Doc</Link>
-							</li>
-							<li>
-								<Link to="/opportunities/scholarship">Scholarships</Link>
-							</li>
-							{/*<li>
-								<Link to="/">Post Doc</Link>
-							</li>*/}
-						</DropdownSection>
-					</div>
-					<HeadingLink noMarginBottom>
-						<Icon /> Faculty
-					</HeadingLink>
-				</ul>
+		<div style={{ width: "40rem" }}>
+			<DropdownSection data-first-dropdown-section noMarginBottom>
+				<div className="d-flex outDropdown">
+					<DropdownSection data-first-dropdown-section noMarginBottom>
+						<h3
+							className="mt-1 ml-3 mb-0"
+							style={{ color: "#6772e5" }}
+						>
+							Students
+						</h3>
+						<div className="d-flex outDropdown">
+							<DropdownSection>
+								<HeadingLink>
+									<Icon />
+									<Link to="/">Internships</Link>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon />
+									<Link to="/opportunities/pg_phd_post_doc">
+										PG/PhD/Post Doc
+									</Link>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon />
+									<Link to="/opportunities/scholarship">
+										Scholarships
+									</Link>
+								</HeadingLink>
+							</DropdownSection>
+						</div>
+					</DropdownSection>
+					<DropdownSection>
+						<h3
+							className="mt-1 ml-3 mb-0"
+							style={{ color: "#6772e5" }}
+						>
+							Faculties
+						</h3>
+						<div className="d-flex outDropdown">
+							<DropdownSection>
+								<HeadingLink>
+									<Icon />
+									<a href="http://www.cefipra.org/">
+										Indo-French
+									</a>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon />
+									<a href="https://www.jsps.go.jp/english/">
+										Indo-Japan
+									</a>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon /> Indo-Germany
+								</HeadingLink>
+								<div className="d-flex flex-column mb-3 mt-1">
+									<a href="https://www.humboldt-foundation.de/en/">
+										Alexander von Humboldt Foundation
+									</a>
+									<a href="https://www.daad.in/en/study-research-in-germany/">
+										DAAD
+									</a>
+									<a href="https://www.igstc.org/">
+										Indo-German Science & Technology Center
+									</a>
+								</div>
+								<HeadingLink>
+									<Icon />
+									<a href="https://www.iusstf.org/">
+										Indo-USA
+									</a>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon />
+									<a href="https://www.ukri.org/our-work/collaborating-internationally/our-international-offices/ukri-india/">
+										Indo-UK
+									</a>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon />
+									<a href="https://aistic.gov.in/ASEAN/HomePage">
+										Indo-ASEAN
+									</a>
+								</HeadingLink>
+								<HeadingLink>
+									<Icon /> Indo-Canada
+								</HeadingLink>
+								<div
+									style={{
+										textAlign: "right",
+										display: "flex",
+										flexDirection: "column",
+									}}
+								>
+									<a href="https://www.shastriinstitute.org/">
+										Indo-Shastri Canadian Institute
+									</a>
+								</div>
+							</DropdownSection>
+						</div>
+					</DropdownSection>
+				</div>
 			</DropdownSection>
-		</OpportunitiesDropdownEl>
+		</div>
+		// <div style={{ width: "20rem" }}>
+
+		// </div>
 	);
 };
 
