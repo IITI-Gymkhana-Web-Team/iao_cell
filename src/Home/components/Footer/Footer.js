381b8c2e00a5fb5b46d1c4fb65a6069e3a58d3bd
gg-dev-05 <garvitgalgat@gmail.com>
2021-04-09 00:04:00 +0530

    social icons animation

--- src/Home/components/Footer/Footer.js
+++ src/Home/components/Footer/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import { iiti_2 } from "../../../assets";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Footer.css";
-import { FaInstagram, FaFacebookSquare, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import {
+	FaInstagram,
+	FaFacebookSquare,
+	FaLinkedin,
+	FaEnvelope,
+} from "react-icons/fa";
 
 export const Footer = () => {
 	return (
@@ -18,44 +23,69 @@ export const Footer = () => {
 							</div>
 						</Col>
 						<Col xs={12} lg={3} className="address mt-0">
-							<h4 className="font-acme">REACH &nbsp;OUT &nbsp;TO &nbsp;US</h4>
+							<h4 className="font-acme">
+								REACH &nbsp;OUT &nbsp;TO &nbsp;US
+							</h4>
 							<div className="addresstext font-ubuntu">
 								{/* Address:{" "} */}
-								<a href="https://goo.gl/maps/fKyPMk9qdPgYG35t7" target="_blank" style={{ color: "white" }}>
-									Indian Institute of Technology Indore, Khandwa Road, Simrol, Indore 453552, INDIA
+								<a
+									href="https://goo.gl/maps/fKyPMk9qdPgYG35t7"
+									target="_blank"
+									style={{ color: "white" }}>
+									Indian Institute of Technology Indore,
+									Khandwa Road, Simrol, Indore 453552, INDIA
 								</a>
-								<div className="phonetext" style={{ color: "white" }}>
+								<div
+									className="phonetext"
+									style={{ color: "white" }}>
 									Phone No:{" "}
-									<a href="tel:1234567890" style={{ color: "white" }}>
+									<a
+										href="tel:1234567890"
+										style={{ color: "white" }}>
 										+91 1234567890
 									</a>
 									<br></br>
-									Email: <a href="mailto:office@iiti.ac.in">office@iiti.ac.in</a>
+									Email:{" "}
+									<a href="mailto:office@iiti.ac.in">
+										office@iiti.ac.in
+									</a>
 									<br></br>
-									<a href="https://goo.gl/maps/qmEGMw6uednJPnGd8">(22.5204° N, 75.9207° E)</a>
+									<a href="https://goo.gl/maps/qmEGMw6uednJPnGd8">
+										(22.5204° N, 75.9207° E)
+									</a>
 								</div>
 							</div>
 						</Col>
 						<Col xs={12} lg={3}>
 							<div className="links mt-0 font-ubuntu">
 								<h4 className="font-acme">QUICK LINKS</h4>
-								<a target="_blank" href="http://rnd.iiti.ac.in/">
+								<a
+									target="_blank"
+									href="http://rnd.iiti.ac.in/">
 									Research
 								</a>
 								<br></br>
-								<a target="_blank" href="http://iiti.ac.in/page/administration">
+								<a
+									target="_blank"
+									href="http://iiti.ac.in/page/administration">
 									Administration
 								</a>
 								<br></br>
-								<a target="_blank" href="http://library.iiti.ac.in/">
+								<a
+									target="_blank"
+									href="http://library.iiti.ac.in/">
 									Library
 								</a>
 								<br></br>
-								<a target="_blank" href="http://iiti.ac.in/resources-generation">
+								<a
+									target="_blank"
+									href="http://iiti.ac.in/resources-generation">
 									Resource Generation
 								</a>
 								<br></br>
-								<a target="_blank" href="http://people.iiti.ac.in/~sic/">
+								<a
+									target="_blank"
+									href="http://people.iiti.ac.in/~sic/">
 									SIC
 								</a>
 							</div>
@@ -64,16 +94,16 @@ export const Footer = () => {
 							<h4 className="font-acme">SOCIAL MEDIA</h4>
 							<div>
 								<a href="#" target="_blank">
-									<FaInstagram />
+									<FaInstagram className="pop-out" />
 								</a>
 								<a href="#/" target="_blank">
-									<FaFacebookSquare />
+									<FaFacebookSquare className="pop-out" />
 								</a>
 								<a href="#" target="_blank">
-									<FaLinkedin />
+									<FaLinkedin className="pop-out" />
 								</a>
 								<a href="#" target="_blank">
-									<FaEnvelope />
+									<FaEnvelope className="pop-out" />
 								</a>
 							</div>
 						</Col>
