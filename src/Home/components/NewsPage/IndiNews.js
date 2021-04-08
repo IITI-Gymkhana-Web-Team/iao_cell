cbb375082981963deb37527d375a0d4a2cc0125a
gg-dev-05 <garvitgalgat@gmail.com>
2021-04-08 23:31:35 +0530

    footer fix for news

--- src/Home/components/NewsPage/IndiNews.js
+++ src/Home/components/NewsPage/IndiNews.js
@@ -11,14 +11,22 @@ export const IndiNews = ({ match }) => {
 
 	if (id >= 0 && id < data.length)
 		return (
-			<div className="indiEvent font-ubuntu container-fluid">
+			<div
+				className="indiEvent font-ubuntu container-fluid"
+				style={{
+					minHeight: "50vh",
+					padding: "2%",
+				}}>
 				<Link to="/outreach/news">
 					<button className="backBtn btn btn-light">
 						<BiArrowBack /> Back
 					</button>
 				</Link>
 				<div className="eventPart1 mt-4 mb-5">
-					<img src={data[id].image} style={{ minWidth: "400px" }} />
+					<img
+						src={data[id].image}
+						style={{ minWidth: "400px", minHeight: "250px" }}
+					/>
 					<div className="eventPart2">
 						<h2 className="font-acme">{data[id].title}</h2>
 						<p className="text-muted">{data[id].date}</p>
