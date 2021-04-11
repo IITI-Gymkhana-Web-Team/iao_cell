8512bbaa88920da4fb0ce65d60e2d24ce625f396
vaibhav <chandravaibhav65@gmail.com>
2021-04-11 17:16:37 +0530

    NAT component added

--- src/Home/pages/Homepage/HomePage.js
+++ src/Home/pages/Homepage/HomePage.js
@@ -3,13 +3,14 @@ import React from "react";
 // import { NavBar } from "./components/NavBar/NavBar";
 import { Carousell } from "../../components/Carousell/Carousell";
 import { AboutUs } from "../../components/AboutUs/AboutUs";
-import { News } from "../../components/News/News";
+// import { News } from "../../components/News/News";
 import { Events } from "../../components/Events/Events";
 // import { Links } from "./components/Links";
 import { Maps } from "../../components/Maps/Maps";
 import { MHRD } from "../../components/MHRD/MHRD";
 import Message from "../../components/Message/Message";
 import "./Home.css";
+import { NAT } from "../../components/NAT/NAT";
 
 export const HomePage = () => {
 	return (
@@ -20,11 +21,11 @@ export const HomePage = () => {
 			<div className="container-fluid">
 				<AboutUs />
 				<Message />
-				<div className="newsEvents container mt-5">
+				{/* <div className="newsEvents container mt-5">
 					<News />
 					<Events />
-				</div>
-				{/*<Maps />*/}
+				</div> */}
+				<NAT />
 				<MHRD />
 			</div>
 		</div>
--- src/Home/reusables/Newsutil.js
+++ src/Home/reusables/Newsutil.js
@@ -3,23 +3,28 @@ import { Card } from "react-bootstrap";
 import { chem_talks_news } from "../../assets";
 import { nsd2020 } from "../../assets";
 import { Link } from "react-router-dom";
+import img from '../../assets/abhijeet.webp'
+
 
 export default function ({ News, index }) {
 	return (
 		<div>
 			<Card className="newsCard">
 				<div className="img-holder">
-					<Card.Img variant="top" src={News["image"]} />
+					<Card.Img variant="top" src={News.image} />
 				</div>
 				<Card.Body>
 					<Card.Title style={styles.title} className="font-acme">
-						<Link to={"/outreach/news/" + index}>
-							{News["title"]}
+						<Link to={"/outreach/news" + index}>
+							{News.title}
 						</Link>
 					</Card.Title>
-					<p className="text-muted font-ubuntu">{News["date"]}</p>
+					{/* <p className="text-muted font-ubuntu">27-10-10</p> */}
 					<Card.Text className="font-ubuntu" style={styles.text}>
-						{News["text"].slice(0, 200) + "..."}
+						lorem lorem lorem lorem
+						lorem lorem lorem lorem
+						lorem lorem lorem lorem
+						lorem lorem lorem lorem
 					</Card.Text>
 				</Card.Body>
 			</Card>
