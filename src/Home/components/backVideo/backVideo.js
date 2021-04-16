3adf7ab468be8cce908540f0681eb3ab5318f5e5
pallavi1812 <upallavi1812@gmail.com>
2021-04-16 17:12:14 +0530

    rankings added

--- src/Home/components/backVideo/backVideo.js
+++ src/Home/components/backVideo/backVideo.js
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+import { data } from "./data";
+import CountUp from 'react-countup';
+import { Col, Row, Card } from 'react-bootstrap';
 import videoo from "../../../assets/noaudio_video2.mp4";
+import { GrStar } from 'react-icons/gr';
 import "./backVideo.css";
 
 export const BackVideo = () => {
@@ -16,7 +20,30 @@ export const BackVideo = () => {
 				<source src={videoo} type="video/mp4" />
 			</video>
 			<div class="content">
-				<p>WELCOME TO IIT INDORE!</p>
+				<Row>
+					{data.map((value) => {
+						return (
+							<Col lg={4}>
+								<Card className="text-center m-4 cardBox" style={{ backgroundColor: 'transparent' }}>
+									<Card.Body className="rankCard">
+										<Card.Title>
+											<GrStar color="#ffd700" />
+											<GrStar color="#ffd700" />
+											<GrStar color="#ffd700" />
+											<GrStar color="#ffd700" />
+											<div className="circle"><CountUp className="rank" end={value.Ranking} duration={8} /></div>
+											<GrStar color="#ffd700" />
+											<GrStar color="#ffd700" />
+											<GrStar color="#ffd700" />
+											<GrStar color="#ffd700" />
+										</Card.Title>
+										<Card.Text className="rankText">{value.Heading}</Card.Text>
+									</Card.Body>
+								</Card>
+							</Col>
+						)
+					})}
+				</Row>
 			</div>
 		</div>
 	);
---
+++ src/Home/components/backVideo/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        Ranking: '3',
+        Heading: 'The World University Ranking 2020 India'
+    },
+    {
+        Ranking: '10',
+        Heading: 'NIRF 2020 Engineering'
+    },
+    {
+        Ranking: '23',
+        Heading: 'NIRF 2020 Overall'
+    }
+]
