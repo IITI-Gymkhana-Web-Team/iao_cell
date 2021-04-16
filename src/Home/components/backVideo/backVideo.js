0bbd03e2c667b584a67a6a22080daa8c8c72e737
gg-dev-05 <garvitgalgat@gmail.com>
2021-04-16 10:21:22 +0530

    audio stripped

--- src/Home/components/backVideo/backVideo.js
+++ src/Home/components/backVideo/backVideo.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import videoo from "../../../assets/video2.mp4";
+import videoo from "../../../assets/noaudio_video2.mp4";
 import "./backVideo.css";
 
 export const BackVideo = () => {
 	return (
 		<div class="video-container">
-			<video autoPlay muted loop id="myVideo">
+			<video
+				controls
+				disablePictureInPicture
+				autoPlay
+				muted
+				loop
+				controlsList="nodownload"
+				id="myVideo">
 				<source src={videoo} type="video/mp4" />
 			</video>
 			<div class="content">
