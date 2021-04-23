8ef3d3ac07fa299ef786ccd7feae9945766e1543
Kushaan Gowda <kushaangowda@yahoo.com>
2021-04-23 21:59:26 +0530

    fixed offer stuff

--- src/Home/components/Message/Message.js
+++ src/Home/components/Message/Message.js
@@ -7,39 +7,30 @@ export default function Message() {
 	return (
 		<>
 			<ScrollAnimation animateIn="slideInRight" animateOnce="true">
-				<div
-					className="container mt-3 messageBox"
-					style={{ borderRadius: "10px" }}>
+				<div className="container mt-3 messageBox" style={{ borderRadius: "10px" }}>
 					<div className="container-fluid p-3">
-						<img
-							src={neelesh}
-							className="img-thumbnail"
-							alt="img-responsive"
-						/>
+						<img src={neelesh} className="img-thumbnail" alt="img-responsive" />
 						<div className="mb-2">
 							<h1>Message from our Director</h1>
 						</div>
-						<h3 className="font-acme messageBy">
-							Prof. Neelesh Kumar Jain
-						</h3>
+						<h3 className="font-acme messageBy">Prof. Neelesh Kumar Jain</h3>
 						<hr />
 						<div
 							style={{
 								fontSize: "17px",
 								marginTop: "20px",
 								maxWidth: "700px",
-							}}>
+							}}
+						>
 							<div class="quoteBox">
 								<FaQuoteLeft className="fa2" />
 								<div class="text ml-4">
 									<p>
-										Lorem Ipsum is simply dummy text of the
-										printing and typesetting industry. Lorem
-										Ipsum has been the industry's standard
-										dummy text ever since the 1500s, when an
-										unknown printer took a galley of type
-										and scrambled it to make a type specimen
-										book.
+										Lorem Ipsum is simply dummy text of the printing and
+										typesetting industry. Lorem Ipsum has been the industry's
+										standard dummy text ever since the 1500s, when an unknown
+										printer took a galley of type and scrambled it to make a
+										type specimen book.
 									</p>
 								</div>
 								<FaQuoteRight className="fa1" />
@@ -49,39 +40,30 @@ export default function Message() {
 				</div>
 			</ScrollAnimation>
 			<ScrollAnimation animateIn="slideInLeft" animateOnce="true">
-				<div
-					className="container mt-3 messageBox"
-					style={{ borderRadius: "10px" }}>
+				<div className="container mt-3 messageBox" style={{ borderRadius: "10px" }}>
 					<div className="container-fluid p-3">
-						<img
-							src={av2}
-							className="img-thumbnail"
-							alt="img-responsive"
-						/>
+						<img src={av2} className="img-thumbnail" alt="img-responsive" />
 						<div className="mb-2">
-							<h1>Message from our Director</h1>
+							<h1>Message from our Dean</h1>
 						</div>
-						<h3 className="font-acme messageBy">
-							Dr. Avinash Sonawane
-						</h3>
+						<h3 className="font-acme messageBy">Dr. Avinash Sonawane</h3>
 						<hr />
 						<div
 							style={{
 								fontSize: "17px",
 								marginTop: "20px",
 								maxWidth: "700px",
-							}}>
+							}}
+						>
 							<div class="quoteBox">
 								<FaQuoteLeft className="fa2" />
 								<div class="text ml-4">
 									<p>
-										Lorem Ipsum is simply dummy text of the
-										printing and typesetting industry. Lorem
-										Ipsum has been the industry's standard
-										dummy text ever since the 1500s, when an
-										unknown printer took a galley of type
-										and scrambled it to make a type specimen
-										book.
+										Lorem Ipsum is simply dummy text of the printing and
+										typesetting industry. Lorem Ipsum has been the industry's
+										standard dummy text ever since the 1500s, when an unknown
+										printer took a galley of type and scrambled it to make a
+										type specimen book.
 									</p>
 								</div>
 								<FaQuoteRight className="fa1" />
