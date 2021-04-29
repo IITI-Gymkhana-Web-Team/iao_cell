9948ee7b853f63783f0679d0f5ed3fb62b44d238
Kushaan Gowda <kushaangowda@yahoo.com>
2021-04-29 08:16:11 +0530

    changed counter ka bg and acme -> roboto

--- src/Home/components/Counter/Counter.js
+++ src/Home/components/Counter/Counter.js
@@ -6,6 +6,7 @@ import { GrStar } from "react-icons/gr";
 import "./Counter.css";
 import ScrollAnimation from "react-animate-on-scroll";
 import VisibilitySensor from "react-visibility-sensor";
+import wheat from "../../../assets/wheat.png";
 
 const Counter = () => {
 	return (
@@ -15,43 +16,51 @@ const Counter = () => {
 					{data.map((value, index) => {
 						return (
 							<Col lg={4} key={index}>
-								<div>
-									<Card
-										className="text-center m-4 rankCardBox"
-										style={{ backgroundColor: "transparent", border: "none" }}
+								<div
+									className="text-center cardBox"
+									style={{
+										backgroundColor: "transparent",
+									}}
+								>
+									<div
+										style={{
+											display: "flex",
+											justifyContent: "center",
+											alignItems: "center",
+										}}
 									>
-										<Card.Body className="rankCards">
-											<Card.Title>
-												<GrStar color="#ffd700" />
-												<GrStar color="#ffd700" />
-												<GrStar color="#ffd700" />
-												<GrStar color="#ffd700" />
-												<div className="cardCircle">
-													<CountUp
-														className="rank"
-														end={value.Ranking}
-														duration={5}
-													>
-														{({ countUpRef, start }) => (
-															<VisibilitySensor
-																onChange={start}
-																delayedCall
-															>
-																<span ref={countUpRef} />
-															</VisibilitySensor>
-														)}
-													</CountUp>
-												</div>
-												<GrStar color="#ffd700" />
-												<GrStar color="#ffd700" />
-												<GrStar color="#ffd700" />
-												<GrStar color="#ffd700" />
-											</Card.Title>
-											<Card.Text className="rankHeading text-center">
-												{value.Heading}
-											</Card.Text>
-										</Card.Body>
-									</Card>
+										<img
+											src={wheat}
+											style={{
+												maxWidth: "min(200px,80vw)",
+												position: "absolute",
+											}}
+										/>
+										<div
+											style={{
+												display: "flex",
+											}}
+										>
+											<h1>#&nbsp;</h1>
+											<h1>
+												<CountUp
+													className="rank"
+													end={value.Ranking}
+													duration={4}
+												>
+													{({ countUpRef, start }) => (
+														<VisibilitySensor
+															onChange={start}
+															delayedCall
+														>
+															<span ref={countUpRef} />
+														</VisibilitySensor>
+													)}
+												</CountUp>
+											</h1>
+										</div>
+									</div>
+									<p className="rankText text-center mt-5">{value.Heading}</p>
 								</div>
 							</Col>
 						);
