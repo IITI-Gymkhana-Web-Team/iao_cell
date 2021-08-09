37bba88eec2cff7d79b10e57aa9bd2b8a5d9d525
Kushaan Gowda <kushaangowda@yahoo.com>
2021-08-09 11:13:13 +0530

    fixed warnings

--- src/Home/components/EmbedVideo/EmbedVideo.js
+++ src/Home/components/EmbedVideo/EmbedVideo.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./EmbedVideo.css";
 
 const EmbedVideo = () => {
