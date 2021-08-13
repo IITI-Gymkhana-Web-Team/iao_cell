bb9b6d668bcb5fc94a0c546702e1f5c6df1a9d92
Kushaan Gowda <kushaangowda@yahoo.com>
2021-08-13 20:41:17 +0530

    fixed some stuff

--- src/Home/components/CustomNavbar/NavOutreach.js
+++ src/Home/components/CustomNavbar/NavOutreach.js
@@ -8,40 +8,70 @@ export const NavOutreach = ({ handleLinkClick }) => {
 
 	return (
 		<div className="mainLink">
-			<div style={{ maxHeight: "40vh" }} className="d-flex justify-content-center align-items-center">
-				<div style={{ minWidth: "100%" }}>
-					<div className="d-flex flex-wrap outreachTab" style={{ justifyContent: "space-evenly" }}>
-						<div className="outreachDivs">
-							<Link to="/" onClick={linkClick}>
-								<p className="navHeading nh1">Conference & Seminars</p>
+			<div style={{ maxHeight: "80vh" }}>
+				<div style={{ maxWidth: "300px" }}>
+					<p className="navHeading nh1">Lecture Series</p>
+					<div>
+						<p>
+							<Link onClick={linkClick} to="/Pros/visitors_experience">
+								Nobel Laureate Lectures
 							</Link>
-						</div>
-						<div className="outreachDivs">
-							<Link to="/" onClick={linkClick}>
-								<p className="navHeading nh1">Public Lectures</p>
+						</p>
+						<p>
+							<Link onClick={linkClick} to="/Pros/places">
+								Frontiers in Physics
 							</Link>
-						</div>
-						<div className="outreachDivs">
-							<Link to="/" onClick={linkClick}>
-								<p className="navHeading nh1">National Science Day</p>
+						</p>
+						<p>
+							<Link onClick={linkClick} to="/int/students">
+								Hardy Ramanujan Lecture Series
 							</Link>
-						</div>
-						<div className="outreachDivs">
-							<Link to="/" onClick={linkClick}>
-								<p className="navHeading nh1">Engagement with Engineering Colleges</p>
-							</Link>
-						</div>
-						<div className="outreachDivs">
-							<Link to="/outreach/events" onClick={linkClick}>
-								<p className="navHeading nh1">Events & Activities</p>
-							</Link>
-						</div>
-						<div className="outreachDivs">
-							<Link to="/" onClick={linkClick}>
-								<p className="navHeading nh1">Visits of School Kids</p>
-							</Link>
+						</p>
+						<div>
+							<p className="navHeading nh2 mt-3">Public Lectures</p>
+							<div>
+								<p>
+									<Link onClick={linkClick} to="/Pros/visitors_experience">
+										Automotive Light-weighting Technologies
+									</Link>
+								</p>
+								<p>
+									<Link onClick={linkClick} to="/Pros/places">
+										Prosperity Through Science Technology
+									</Link>
+								</p>
+								<p>
+									<Link onClick={linkClick} to="/int/students">
+										Role of Science in Nation Building during Global Health Crisis
+									</Link>
+								</p>
+							</div>
 						</div>
 					</div>
+				</div>
+				<div>
+					<p className="navHeading nh1">Conferences and Seminars</p>
+					<div>
+						<p>
+							<a href="http://rnd.iiti.ac.in/" target="_blank" rel="noreferrer">
+								Chemistry Talks
+							</a>
+						</p>
+						<p>
+							<a href="https://academic.iiti.ac.in/" target="_blank" rel="noreferrer">
+								Nobel Prizes-2020
+							</a>
+						</p>
+					</div>
+				</div>
+				<div>
+					<p className="navHeading nh1">National Science Day</p>
+				</div>
+				<div>
+					<p className="navHeading nh1">Visits of School Kids</p>
+				</div>
+				<div>
+					<p className="navHeading nh1">Engagement with Engineering Colleges</p>
 				</div>
 			</div>
 		</div>
