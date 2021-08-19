77832f4cecaee6aa74c1f9bd217dda43d8d0bc1b
Kushaan Gowda <kushaangowda@yahoo.com>
2021-08-19 11:33:05 +0530

    changed router to hash router

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { HomePage } from "./pages/Homepage/HomePage";
 import { People } from "./pages/Peoplepage/People";
 import { Title } from "./components/Title/Title";
