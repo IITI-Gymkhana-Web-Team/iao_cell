2b7d077a0235b6d8adcf39ab18275b84dfab98a9
pallavi1812 <upallavi1812@gmail.com>
2021-08-30 12:55:05 +0530

    updated news and activities

--- src/Home/components/CarousellCards/CarousellCards.js
+++ src/Home/components/CarousellCards/CarousellCards.js
@@ -5,15 +5,26 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import { Col, Container, Row } from "react-bootstrap";
 import ScrollAnimation from "react-animate-on-scroll";
-import { nsd2020, chem_talks_news, ai, d2, g, chemR, phyR } from "../../../assets";
+import { chemR, phyR } from "../../../assets";
+import NSDImg from "../../../assets/NSDImg.jpeg";
+import FrenchConsultImg from "../../../assets/FrenchConsultationVisit.jpeg";
+import BUL from "../../../assets/BUL.jpeg";
+import NIDB from "../../../assets/NIDB&IITI.jpeg";
+import MoU from "../../../assets/MoUwithUoAIITI.jpeg";
 
 const CarousellCards = () => {
 	const data1 = [
 		{
-			img: chemR,
-			title: "Direct H2O2 synthesis",
-			desc: "Catalysis group, IIT Indore and KIT Germany reported their work on shape-selective Pd3Pb nanocrystals based catalysts for direct H2O2 synthesis. Their findings inferred the tuning in electronic structure and lack of Pd ensembles retarted O-O cleavage.",
-			link: "https://pubs.acs.org/doi/abs/10.1021/acscatal.0c03561",
+			img: BUL,
+			title: "Forecasting crude oil futures prices by Prof. Date",
+			desc: "Dr. Paresh Date, Reader and Director of Research in Mathematics, Brunel University, London, UK has given talk on “Forecasting crude oil futures prices using the Kalman filter and news sentiment” on August 27, 2021 under IAO International Outreach event of IIT Indore. The talk was coordinated by Dr. Abhinoy K. Singh, Department of Electrical Engineering, IIT Indore.",
+			link: "https://www.brunel.ac.uk/people/paresh-date",
+		},
+		{
+			img: NIDB,
+			title: "National Institute of Design Bhopal visited IIT Indore",
+			desc: "Officials from National Institute of Design, Bhopal visited International Affairs & Outreach office, IIT Indore on August 23, 2021. During the visit, we had a constructive discussion with NID team to extend our support to establish international office at NID Bhopal. ",
+			link: "",
 		},
 		{
 			img: phyR,
@@ -24,42 +35,30 @@ const CarousellCards = () => {
 	];
 	const data2 = [
 		{
-			img: nsd2020,
-			title: "Lorem Ipsum",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			link: "http://astronomy.iiti.ac.in/",
+			img: FrenchConsultImg,
+			title: "French Consulate General in Mumbai visited IIT Indore",
+			desc: "Mrs. Sonia Barbry, French Consulate General in Mumbai and Mr. Thomas Simoes, Director, Alliance Francaise, Bhopal visited IIT Indore on March 18, 2021, and had fruitful discussions on escalating and strengthening the bilateral research work, exchange of students and faculties and many more aspects. ",
+			link: "",
 		},
 		{
-			img: d2,
-			title: "Lorem Ipsum",
-			desc: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-			link: "http://bsbe.iiti.ac.in/",
-		},
-		{
-			img: chem_talks_news,
-			title: "Lorem Ipsum",
-			desc: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-			link: "http://chemistry.iiti.ac.in/",
-		},
+			img: NSDImg,
+			title: "IIT Indore celebrated National Science Day in virtual mode",
+			desc: "IIT Indore is celebrating the National Science Day on Feb. 28, 2021, where several activities such as daily life physics experiments, chemical illusion, journey through cosmos, mathematical magic, biology talk, will be organized for school kids in virtual mode.",
+			link: "https://www.freepressjournal.in/indore/indore-iit-indore-to-celebrate-national-science-day-in-virtual-mode",
+		}
 	];
 	const data3 = [
 		{
-			img: nsd2020,
-			title: "Lorem Ipsum",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			link: "http://astronomy.iiti.ac.in/",
+			img: MoU,
+			title: "MoU signed with University of Alberta, Canada",
+			desc: "IIT Indore and University of Alberta, Canada has signed an MOU to escalate the research activities and exchange on the mutually agreeable domains, covering the exchange of students, faculties and staffs, development of joint curriculum, organising joint workshops and conferences, and also to explore the possibilities of joint degree programmes. This MOU has been signed in 2021 and will remain valid for 5 years.",
+			link: "https://clt1249866.bmeurl.co/C8589D3",
 		},
 		{
-			img: ai,
-			title: "Lorem Ipsum",
-			desc: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-			link: "http://bsbe.iiti.ac.in/",
-		},
-		{
-			img: g,
-			title: "Lorem Ipsum",
-			desc: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-			link: "http://chemistry.iiti.ac.in/",
+			img: chemR,
+			title: "Direct H2O2 synthesis",
+			desc: "Catalysis group, IIT Indore and KIT Germany reported their work on shape-selective Pd3Pb nanocrystals based catalysts for direct H2O2 synthesis. Their findings inferred the tuning in electronic structure and lack of Pd ensembles retarted O-O cleavage.",
+			link: "https://pubs.acs.org/doi/abs/10.1021/acscatal.0c03561",
 		},
 	];
 
--- src/Home/components/NewsPage/data.js
+++ src/Home/components/NewsPage/data.js
@@ -1,22 +1,69 @@
-import { nsd2020, chem_talks_news, ai } from "../../../assets";
+import { nsd2020, chem_talks_news, ai, chemR, phyR } from "../../../assets";
+import NSDImg from "../../../assets/NSDImg.jpeg";
+import FrenchConsultImg from "../../../assets/FrenchConsultationVisit.jpeg";
+import BUL from "../../../assets/BUL.jpeg";
+import NIDB from "../../../assets/NIDB&IITI.jpeg";
+import MoU from "../../../assets/MoUwithUoAIITI.jpeg";
 
 export const data = [
+	{
+		image: BUL,
+		title: "Forecasting crude oil futures prices by Prof. Date",
+		desc: "Dr. Paresh Date, Reader and Director of Research in Mathematics, Brunel University, London, UK has given talk on “Forecasting crude oil futures prices using the Kalman filter and news sentiment” on August 27, 2021 under IAO International Outreach event of IIT Indore. The talk was coordinated by Dr. Abhinoy K. Singh, Department of Electrical Engineering, IIT Indore.",
+		link: "https://www.brunel.ac.uk/people/paresh-date",
+	},
+	{
+		image: NIDB,
+		title: "National Institute of Design Bhopal visited IIT Indore",
+		desc: "Officials from National Institute of Design, Bhopal visited International Affairs & Outreach office, IIT Indore on August 23, 2021. During the visit, we had a constructive discussion with NID team to extend our support to establish international office at NID Bhopal. ",
+		link: "",
+	},
+	{
+		image: FrenchConsultImg,
+		title: "French Consulate General in Mumbai visited IIT Indore",
+		desc: "Mrs. Sonia Barbry, French Consulate General in Mumbai and Mr. Thomas Simoes, Director, Alliance Francaise, Bhopal visited IIT Indore on March 18, 2021, and had fruitful discussions on escalating and strengthening the bilateral research work, exchange of students and faculties and many more aspects. ",
+		link: "",
+	},
+	{
+		image: NSDImg,
+		title: "IIT Indore celebrated National Science Day in virtual mode",
+		desc: "IIT Indore is celebrating the National Science Day on Feb. 28, 2021, where several activities such as daily life physics experiments, chemical illusion, journey through cosmos, mathematical magic, biology talk, will be organized for school kids in virtual mode.",
+		link: "https://www.freepressjournal.in/indore/indore-iit-indore-to-celebrate-national-science-day-in-virtual-mode",
+	},
+	{
+		image: MoU,
+		title: "MoU signed with University of Alberta, Canada",
+		desc: "IIT Indore and University of Alberta, Canada has signed an MOU to escalate the research activities and exchange on the mutually agreeable domains, covering the exchange of students, faculties and staffs, development of joint curriculum, organising joint workshops and conferences, and also to explore the possibilities of joint degree programmes. This MOU has been signed in 2021 and will remain valid for 5 years.",
+		link: "https://clt1249866.bmeurl.co/C8589D3",
+	},
+	{
+		image: chemR,
+		title: "Direct H2O2 synthesis",
+		desc: "Catalysis group, IIT Indore and KIT Germany reported their work on shape-selective Pd3Pb nanocrystals based catalysts for direct H2O2 synthesis. Their findings inferred the tuning in electronic structure and lack of Pd ensembles retarted O-O cleavage.",
+		link: "https://pubs.acs.org/doi/abs/10.1021/acscatal.0c03561",
+	},
+	{
+		image: phyR,
+		title: "Origin of natural and magnetic field induced polar order",
+		desc: "The group of Dr. Sagdeo, IIT Indore and STFC Rutherford Appleton Laboratory, UK have reported the possible origin of natural and magnetic field induced switchable polar order near room temperature (RT) in PrFe1/2Cr1/2O3",
+		link: "https://journals.aps.org/prb/abstract/10.1103/PhysRevB.104.035101",
+	},
 	{
 		title: "Impact of the Digital Revolution on Higher Education",
 		image: ai,
 		desc: 'Dr. Aruna Tiwari, Associate Professor, CSE represented IIT Indore as an Artificial Intelligence expert in the Internationalist Podcast series on  "Impact of the digital revolution on higher education", organized by the Association of Commonwealth Universities, UK on June 8, 2021.',
-		date: "June 8, 2021",
+		// date: "June 8, 2021",
 	},
 	{
 		title: "National Science Day",
 		image: nsd2020,
 		desc: "National Science Day 2020 was organised at IIT Indore  on Feb. 28, 2020, where over 90 students participated and fun learned science with interesting science experiments, demonstrations, talks and various fun activities. ",
-		date: "February 28, 2020",
+		// date: "February 28, 2020",
 	},
 	{
 		title: "Chemistry Talks",
 		image: chem_talks_news,
 		desc: " Chemistry Talks: India celebrates the National Science Day on 28th February to mark the discovery of the Raman effect by Sir C. V. Raman On this occasion, the Department of Chemistry ,Indian Institute of Technology Indore is organizing Chemistry Talks.In the month of February, chemistry-based research talks from several eminent speakers across the world have been scheduled.\nChemistry Lectures aim to provide a platform for eminent chemists from all over the world to share and discuss wider aspects of the domain of chemical sciences and allied areas. The webinar is expected to provide a unique opportunity to establish new and ever-lasting bonding between the researchers, and inspire the young students to explore new ways to understand chemistry and its interface.",
-		date: "January 28, 2021",
+		// date: "January 28, 2021",
 	},
 ];
