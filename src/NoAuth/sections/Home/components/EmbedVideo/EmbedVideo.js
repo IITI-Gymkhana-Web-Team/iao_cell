b6f129a97974c3ebc884069340b003bc90b55fdf
Kushaan Gowda <kushaangowda@yahoo.com>
2021-10-10 14:44:54 +0530

    fixed embed video

--- src/NoAuth/sections/Home/components/EmbedVideo/EmbedVideo.js
+++ src/NoAuth/sections/Home/components/EmbedVideo/EmbedVideo.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
-// import { Link } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import "./EmbedVideo.css";
 
 const announcements = [
@@ -20,46 +19,75 @@ const announcements = [
 
 const EmbedVideo = () => {
 	const mainRef = useRef(null);
+	const [allowScroll, setAllowScroll] = useState(true);
+
 	useEffect(() => {
-		if (mainRef.current) {
+		if (mainRef.current && allowScroll) {
 			var pos = 30;
 			const interval = setInterval(() => {
-				if (mainRef.current) {
+				if (mainRef.current && allowScroll) {
 					mainRef.current.scrollTop = pos;
 					if (pos === 164) pos = 0;
 					else pos += 1;
 				}
-			}, 2);
+			}, 10);
 			return () => clearInterval(interval);
 		}
+	}, [allowScroll]);
+
+	const [width, setWidth] = useState(600);
+
+	const updateDimensions = () => {
+		// setWidth(window.innerWidth);
+		console.log(`width: ${window.innerWidth}`);
+		if (window.innerWidth >= 1000) {
+			setWidth(600);
+		} else {
+			setWidth(0.9 * window.innerWidth);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", updateDimensions);
+		return () => window.removeEventListener("resize", updateDimensions);
 	}, []);
 
 	return (
-		<Row className="embed-video container m-auto">
-			<Col md="12" lg="8" className="embed-video-div m-0 p-0" style={{ marginTop: "100px !important" }}>
-				<div className="video-responsive">
-					<iframe
-						className="embed-iframe"
-						src="https://www.youtube.com/embed/Thjsxz31rRk"
-						title="YouTube video player"
-						frameborder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen
-					></iframe>
-				</div>
-			</Col>
-			<Col md="12" lg="4" style={{ paddingRight: "0" }}>
+		<div className="embed-video container d-flex flex-wrap">
+			<div className="embed-video-div m-0 p-0" style={{ marginTop: "100px !important" }}>
+				<iframe
+					className="embed-iframe"
+					src="https://www.youtube.com/embed/Thjsxz31rRk"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+					width={width}
+					height={(9 / 16) * width}
+				></iframe>
+			</div>
+			<div style={{ paddingRight: "0" }}>
 				<div className="announce">
 					<p className="announce-title text-left font-acme">Announcements</p>
 					<hr />
-					<ListGroup variant="flush" ref={mainRef} className="announce-text mt-2">
+					<ListGroup
+						variant="flush"
+						ref={mainRef}
+						className="announce-text mt-2"
+						onMouseEnter={() => {
+							setAllowScroll(false);
+						}}
+						onMouseLeave={() => {
+							setAllowScroll(true);
+						}}
+					>
 						{announcements.map((item) => (
 							<ListGroupItem key={item.id}>{item.desc}</ListGroupItem>
 						))}
 					</ListGroup>
 				</div>
-			</Col>
-		</Row>
+			</div>
+		</div>
 	);
 };
 
